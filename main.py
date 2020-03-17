5f53ad15809a7f8aff14002d2cce5e9e21ec6e0a
tomdotscott <tom.scott45456@gmail.com>
2020-03-17 16:39:24 +0000

    Email working

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import requests
+import smtplib
 from bs4 import BeautifulSoup
+
+def send_mail(tableData):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login('email', 'password')
+    subject = f"Coronavirus Cases in {tableData[0]} today"
+    body = f"Today in: {tableData[0]}\nTotal Cases: {tableData[1]}\nNew Cases: {tableData[2]} \
+           '\nTotal Deaths: {tableData[3]}\nNew Deaths: {tableData[4]}\nRecovered: + {tableData[5]}\
+           '\nActive Cases: {tableData[6]}\nCheck the link: https://www.worldometers.info/coronavirus"
+    message = f"Subject: {subject}\n\n{body}"
+    server.sendmail('Coronavirus', 'emailtosendto', message)
+    print(body)
+
+    print("Email has been sent!")
+    server.quit()
+
+
 
 url = "https://www.worldometers.info/coronavirus/"
 
@@ -19,4 +39,6 @@ for row in rows:
     if cols[0] == 'UK':
         UK = cols
 
-print(UK)
+send_mail(list(UK))
+
+
