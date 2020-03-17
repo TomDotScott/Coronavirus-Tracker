be2ba6b51981f68985d165c19f713b3027a7be78
tomdotscott <tom.scott45456@gmail.com>
2020-03-17 15:32:59 +0000

    Find data in table

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ webpage = requests.get(url)
 
 soup = BeautifulSoup(webpage.text, 'html.parser')
 
+table_body = soup.find('tbody')
+
+rows = table_body.find_all('tr')
+
+for row in rows:
+    cols = row.find_all('td')
+    cols = [x.text.strip() for x in cols]
+    print(cols)
